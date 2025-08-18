ac18f5975500dac3fe197de34954c8944eca3738
fewioaghwrao <nrk13959@gmail.com>
2025-08-19 05:49:14 +0900

    Add files via upload

---
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,16 @@
+package kadai_021;
+
+public class DictionaryExec_Chapter21 {
+
+	public static void main(String[] args) {
+		
+		Dictionary_Chapter21  chapter21 = new Dictionary_Chapter21();
+		chapter21.setDictionary();
+		chapter21.investDictionary("apple");
+		chapter21.investDictionary("banana");
+		chapter21.investDictionary("grape");
+		chapter21.investDictionary("orange");
+
+	}
+
+}
